77cca576c35f10d20795556bfbf1d391717dcc91
febrio-d <7g26febrio@gmail.com>
2022-12-30 21:24:28 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         Admin::create([
-            'name' => 'Febrio',
-            'username' => 'feb',
+            'name' => 'Admin',
+            'username' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', //password
             'phone' => '085856795919',
             'role' => 'admin'
